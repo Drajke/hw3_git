56bbc433c5df51b592df12d4f2a2f7a7571492c5
Drajke <drajke@gmail.com>
2023-01-04 18:23:11 +0300

    change

--- src/test/java/justTest.java
+++ src/test/java/justTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 public class justTest {
     @Test
     void oneTest(){
+        Selenide.open("https://google.com");
         Selenide.open("https://github.com/selenide/selenide");
     }
 }
